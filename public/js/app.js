79e9cf72256b2abfc202c2290e306a76f1c1d764
Sandesh Timilsina <stimilsina@unm.edu>
2020-12-25 08:51:39 -0700

    Heroku setups

--- public/js/app.js
+++ public/js/app.js
@@ -14,8 +14,8 @@ submitLocation.addEventListener('submit', (e)=>{
     const location = search.value
     messageFirst.textContent = 'Waiting for info...'
     messageSecond.textContent = ''
-    console.log(location)
-    fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+    
+    fetch('/weather?address='+ location).then((response)=>{
     response.json().then((data) =>{
         if(data.error){
             messageFirst.textContent = data.error
